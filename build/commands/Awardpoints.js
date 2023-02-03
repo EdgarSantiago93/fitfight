ea5d0bfbd1780a752d59904a8045768b66cc753c
Edgar Santiago <esg.827@gmail.com>
2023-02-03 07:55:39 -0600

    fix for script

--- build/commands/Awardpoints.js
+++ build/commands/Awardpoints.js
@@ -15,12 +15,21 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
     if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -33,53 +42,64 @@ class Awardpoints extends standalone_1.BaseCommand {
         this.logger.info('->FITFIGHT<-');
         const { default: Entry } = await Promise.resolve().then(() => __importStar(global[Symbol.for('ioc.use')]('App/Models/Entry')));
         const yesterday = (0, moment_1.default)().utcOffset(-6).subtract(1, 'days');
-        if ((0, moment_1.default)().utcOffset(-6).hour() < 12 || (0, moment_1.default)().utcOffset(-6).hour() > 1) {
+        if ((0, moment_1.default)().utcOffset(-6).hour() == 0 ||
+            (0, moment_1.default)().utcOffset(-6).hour() == 1 ||
+            this.forceUpdate) {
+            if (this.forceUpdate) {
+                this.logger.info('Running awardpoints with forced flag');
+            }
+            this.logger.info('Yesterday was: ' + yesterday.format());
+            const entriesForTheDay = await Entry.query()
+                .whereNot('status', 'validated')
+                .where('is_rest_day', false)
+                .where('created_at', '>=', yesterday.startOf('day').format())
+                .where('created_at', '<=', yesterday.endOf('day').format());
+            this.logger.info('Awarding points to users for: ' + yesterday.format('YYYY-MM-DD'));
+            this.logger.info(entriesForTheDay.length.toString());
+            for (const entry of entriesForTheDay) {
+                const votes = await entry.related('votes').query();
+                const votesFor = votes.filter((vote) => vote.type === 'for');
+                const votesAgainst = votes.filter((vote) => vote.type === 'against');
+                this.logger.info('Entry: ' + entry.id + ' has ' + votesFor.length + ' votes for');
+                this.logger.info('Entry: ' + entry.id + ' has ' + votesAgainst.length + ' votes against');
+                if (votesFor.length > votesAgainst.length ||
+                    (votesFor.length == 0 && votesAgainst.length == 0)) {
+                    this.logger.success('Entry: ' + entry.id + ' was validated');
+                    entry.status = 'validated';
+                    entry.is_validated = true;
+                    await entry.save();
+                }
+                if (votesFor.length === votesAgainst.length) {
+                    this.logger.action('Entry: ' + entry.id + ' was a tie');
+                    entry.status = 'validated';
+                    entry.is_validated = true;
+                    await entry.save();
+                }
+                if (votesFor.length < votesAgainst.length) {
+                    this.logger.error('Entry: ' + entry.id + ' was rejected');
+                    entry.status = 'rejected';
+                    entry.is_validated = false;
+                    await entry.save();
+                }
+                this.logger.info('');
+            }
+        }
+        else {
             this.logger.info('Not running awardpoints');
             this.logger.info('Current hour: ' + (0, moment_1.default)().utcOffset(-6).hour());
             return;
         }
-        this.logger.info('Yesterday was: ' + yesterday.format());
-        const entriesForTheDay = await Entry.query()
-            .whereNot('status', 'validated')
-            .where('is_rest_day', false)
-            .where('created_at', '>=', yesterday.startOf('day').format())
-            .where('created_at', '<=', yesterday.endOf('day').format());
-        this.logger.info('Awarding points to users for: ' + yesterday.format('YYYY-MM-DD'));
-        this.logger.info(entriesForTheDay.length.toString());
-        for (const entry of entriesForTheDay) {
-            const votes = await entry.related('votes').query();
-            const votesFor = votes.filter((vote) => vote.type === 'for');
-            const votesAgainst = votes.filter((vote) => vote.type === 'against');
-            this.logger.info('Entry: ' + entry.id + ' has ' + votesFor.length + ' votes for');
-            this.logger.info('Entry: ' + entry.id + ' has ' + votesAgainst.length + ' votes against');
-            if (votesFor.length > votesAgainst.length ||
-                (votesFor.length == 0 && votesAgainst.length == 0)) {
-                this.logger.success('Entry: ' + entry.id + ' was validated');
-                entry.status = 'validated';
-                entry.is_validated = true;
-                await entry.save();
-            }
-            if (votesFor.length === votesAgainst.length) {
-                this.logger.action('Entry: ' + entry.id + ' was a tie');
-                entry.status = 'validated';
-                entry.is_validated = true;
-                await entry.save();
-            }
-            if (votesFor.length < votesAgainst.length) {
-                this.logger.error('Entry: ' + entry.id + ' was rejected');
-                entry.status = 'rejected';
-                entry.is_validated = false;
-                await entry.save();
-            }
-            this.logger.info('');
-        }
     }
 }
-exports.default = Awardpoints;
 Awardpoints.commandName = 'awardpoints';
 Awardpoints.description = 'Award points to users for the day';
 Awardpoints.settings = {
     loadApp: true,
     stayAlive: false,
 };
+__decorate([
+    standalone_1.flags.boolean({ alias: 'f', description: 'force' }),
+    __metadata("design:type", Boolean)
+], Awardpoints.prototype, "forceUpdate", void 0);
+exports.default = Awardpoints;
 //# sourceMappingURL=Awardpoints.js.map
--- commands/Awardpoints.ts
+++ commands/Awardpoints.ts
@@ -1,4 +1,4 @@
-import { BaseCommand } from '@adonisjs/core/build/standalone'
+import { BaseCommand, flags } from '@adonisjs/core/build/standalone'
 import moment from 'moment'
 
 export default class Awardpoints extends BaseCommand {
@@ -13,6 +13,9 @@ export default class Awardpoints extends BaseCommand {
     stayAlive: false,
   }
 
+  @flags.boolean({ alias: 'f', description: 'force' })
+  public forceUpdate: boolean
+
   public async run() {
     this.logger.info('->FITFIGHT<-')
     const { default: Entry } = await import('App/Models/Entry')
@@ -21,55 +24,63 @@ export default class Awardpoints extends BaseCommand {
     const yesterday = moment().utcOffset(-6).subtract(1, 'days')
 
     // only run between 12:00am and 1:30am
-    if (moment().utcOffset(-6).hour() < 12 || moment().utcOffset(-6).hour() > 1) {
+
+    if (
+      moment().utcOffset(-6).hour() == 0 ||
+      moment().utcOffset(-6).hour() == 1 ||
+      this.forceUpdate
+    ) {
+      if (this.forceUpdate) {
+        this.logger.info('Running awardpoints with forced flag')
+      }
+      this.logger.info('Yesterday was: ' + yesterday.format())
+
+      const entriesForTheDay = await Entry.query()
+        .whereNot('status', 'validated')
+        .where('is_rest_day', false)
+        .where('created_at', '>=', yesterday.startOf('day').format())
+        .where('created_at', '<=', yesterday.endOf('day').format())
+
+      this.logger.info('Awarding points to users for: ' + yesterday.format('YYYY-MM-DD'))
+      this.logger.info(entriesForTheDay.length.toString())
+
+      for (const entry of entriesForTheDay) {
+        const votes = await entry.related('votes').query()
+        const votesFor = votes.filter((vote) => vote.type === 'for')
+        const votesAgainst = votes.filter((vote) => vote.type === 'against')
+        this.logger.info('Entry: ' + entry.id + ' has ' + votesFor.length + ' votes for')
+        this.logger.info('Entry: ' + entry.id + ' has ' + votesAgainst.length + ' votes against')
+
+        if (
+          votesFor.length > votesAgainst.length ||
+          (votesFor.length == 0 && votesAgainst.length == 0)
+        ) {
+          this.logger.success('Entry: ' + entry.id + ' was validated')
+          entry.status = 'validated'
+          entry.is_validated = true
+          await entry.save()
+        }
+        if (votesFor.length === votesAgainst.length) {
+          this.logger.action('Entry: ' + entry.id + ' was a tie')
+          //TODO: add logic for tie
+
+          entry.status = 'validated'
+          entry.is_validated = true
+          await entry.save()
+        }
+        if (votesFor.length < votesAgainst.length) {
+          this.logger.error('Entry: ' + entry.id + ' was rejected')
+          entry.status = 'rejected'
+          entry.is_validated = false
+          await entry.save()
+        }
+
+        this.logger.info('')
+      }
+    } else {
       this.logger.info('Not running awardpoints')
       this.logger.info('Current hour: ' + moment().utcOffset(-6).hour())
       return
-    }
-
-    this.logger.info('Yesterday was: ' + yesterday.format())
-
-    const entriesForTheDay = await Entry.query()
-      .whereNot('status', 'validated')
-      .where('is_rest_day', false)
-      .where('created_at', '>=', yesterday.startOf('day').format())
-      .where('created_at', '<=', yesterday.endOf('day').format())
-
-    this.logger.info('Awarding points to users for: ' + yesterday.format('YYYY-MM-DD'))
-    this.logger.info(entriesForTheDay.length.toString())
-
-    for (const entry of entriesForTheDay) {
-      const votes = await entry.related('votes').query()
-      const votesFor = votes.filter((vote) => vote.type === 'for')
-      const votesAgainst = votes.filter((vote) => vote.type === 'against')
-      this.logger.info('Entry: ' + entry.id + ' has ' + votesFor.length + ' votes for')
-      this.logger.info('Entry: ' + entry.id + ' has ' + votesAgainst.length + ' votes against')
-
-      if (
-        votesFor.length > votesAgainst.length ||
-        (votesFor.length == 0 && votesAgainst.length == 0)
-      ) {
-        this.logger.success('Entry: ' + entry.id + ' was validated')
-        entry.status = 'validated'
-        entry.is_validated = true
-        await entry.save()
-      }
-      if (votesFor.length === votesAgainst.length) {
-        this.logger.action('Entry: ' + entry.id + ' was a tie')
-        //TODO: add logic for tie
-
-        entry.status = 'validated'
-        entry.is_validated = true
-        await entry.save()
-      }
-      if (votesFor.length < votesAgainst.length) {
-        this.logger.error('Entry: ' + entry.id + ' was rejected')
-        entry.status = 'rejected'
-        entry.is_validated = false
-        await entry.save()
-      }
-
-      this.logger.info('')
     }
   }
 }
